81bd1d9a2678e003ed35249cb35c1e563e6a055a
Wheeler-Christian <christianwheeler505@gmail.com>
2024-03-13 12:27:46 -0600

    Add project files.

---
+++ PiCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PiCalculator
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // PI calculator using the Nilakantha Series
+            // PI =  3 + 4/(2*3*4) - 4/(4*5*6) + 4/(6*7*8) - 4/(8*9*10)
+            Console.WriteLine("PI Calculator");
+
+            decimal sum = 3;
+
+            for(int i = 2; i < 1000; i += 2) { 
+                decimal term = (decimal)4/(i*(i+1)*(i+2));
+                if(i % 4 == 0)
+                {
+                    sum -= term;
+                }
+                else
+                {
+                    sum += term;
+                }
+
+                Console.WriteLine(sum);
+            }
+
+            Console.WriteLine("PI is approximately = " + sum);
+        }
+    }
+}
